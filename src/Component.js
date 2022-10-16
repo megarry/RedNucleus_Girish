89bd1ef0e5ee846a922bafeeeeb2bfc21e39477e
D2_63326_Girish <girishgahandule@gmail.com>
2022-10-17 01:01:05 +0530

    added comments

--- src/Component.js
+++ src/Component.js
@@ -1,48 +1,51 @@
-import profile from './man.png';
-import './Component.css';
-import { useEffect, useState } from 'react';
+import profile from "./man.png";
+import "./Component.css";
+import { useEffect, useState } from "react";
 
 function Component() {
-
 	const [follow, setFollow] = useState(false);
 
-	// $(document).ready(function() {
-	// 	$("p").html((_, html) => {
-	// 	  return html.replace(/(\#\w+)/g, '<span class="blue">$1</span>');
-	// 	});
-	//   });
+	const changeText = () => {
+		let str = document.getElementById("paraContent").innerHTML;
+		let res = str.replace(/(\#\w+)/g, '<span class="blue">$1</span>');
+		document.getElementById("paraContent").innerHTML = res;
+	};
 
-const changeText = () =>{
-	let str = document.getElementById("demo").innerHTML; 
-	let res = str.replace(/(\#\w+)/g, '<span class="blue">$1</span>');
-	document.getElementById("demo").innerHTML = res;
-}
+	useEffect(() => {
+		changeText();
+	}, []);
 
-useEffect(()=> {
-	changeText()
-},[])
-
-  return (
-		<div className = "Card">
-			<div className = "HeaderImg">
-				<img className="Image" src = {profile} alt="Profile" />
+	return (
+		<div className="card">
+			<div className="headerImg">
+				<img className="image" src={profile} alt="Profile" />
 			</div>
-			<div className = "Text">
-				<div className="Button">
-					<button className={follow ? "btnFollow" : "btnUnfollow"} onClick = {() =>setFollow(!follow)} > {follow ? "Unfollow" : "Follow"} </button>
+			<div className="text">
+				<div className="button">
+					<button
+						className={follow ? "btnFollow" : "btnUnfollow"}
+						onClick={() => setFollow(!follow)}
+					>
+						
+						{follow ? "Unfollow" : "Follow"}
+					</button>
 				</div>
-				<div className = "Text-Title">
-					Space Ship
-				</div>
-				<div className = "Text-Content">
-					<p id='demo'>Translating Ideas to Reality - #React, #JAVASCRIPT, #CSS, and #UXDesign</p>
-				<div className='Footer'>
-					<p>North America & Europe &#x2022; hanservices.com &#x2022; Joined March 2009 </p>
-				</div>
+				<div className="textTitle">Space Ship</div>
+				<div className="textContent">
+					<p id="paraContent">
+						Translating Ideas to Reality - #React, #JAVASCRIPT,
+						#CSS, and #UXDesign
+					</p>
+					<div className="footer">
+						<p>
+							North America & Europe &#x2022; hanservices.com
+							&#x2022; Joined March 2009
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
-  );
+	);
 }
 
 export default Component;
