c2af7c0073cb43533cb6187c9662d7d49ed34db3
D2_63326_Girish <girishgahandule@gmail.com>
2022-10-16 19:26:37 +0530

    follow button added

--- src/Component.js
+++ src/Component.js
@@ -1,19 +1,27 @@
 import profile from './man.png';
 import './Component.css';
+import { useState } from 'react';
 
 function Component() {
+
+	const [follow, setFollow] = useState(false);
+
   return (
 		<div className = "Card">
 			<div className = "HeaderImg">
 				<img className="Image" src = {profile} alt="Profile" />
 			</div>
 			<div className = "Text">
+				<div className="Button">
+					<button onClick = {() =>setFollow(!follow)} > {follow ? "Unfollow" : "Follow"} </button>
+				</div>
 				<div className = "Text-Title">
 					Space Ship
 				</div>
 				<div className = "Text-Content">
 					<p>Translating Ideas to Reality - #React, #JAVASCRIPT, #CSS, and #UXDesign</p>
 					<p>North America & Europe &#x2022; hanservices.com &#x2022; Joined March 2009 </p>
+					
 				</div>
 			
 			</div>
