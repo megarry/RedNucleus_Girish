82cbec7632b88a32d706ee91800600bc7ff3173f
D2_63326_Girish <girishgahandule@gmail.com>
2022-10-16 20:26:47 +0530

    made aesthetic changes

--- src/Component.js
+++ src/Component.js
@@ -1,10 +1,17 @@
 import profile from './man.png';
 import './Component.css';
 import { useState } from 'react';
+import $ from 'jquery';
 
 function Component() {
 
 	const [follow, setFollow] = useState(false);
+
+	$(document).ready(function() {
+		$("p").html(function(_, html) {
+		  return html.replace(/(\#\w+)/g, '<span class="blue">$1</span>');
+		});
+	  });
 
   return (
 		<div className = "Card">
@@ -20,7 +27,9 @@ function Component() {
 				</div>
 				<div className = "Text-Content">
 					<p>Translating Ideas to Reality - #React, #JAVASCRIPT, #CSS, and #UXDesign</p>
+				<div className='Footer'>
 					<p>North America & Europe &#x2022; hanservices.com &#x2022; Joined March 2009 </p>
+				</div>
 					
 				</div>
 			
